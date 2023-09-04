4d38c6f65f381573fd72dfa1aff22aca5b0f6b52
Wendel Schimitz <98092597+wendel.martins@compasso.com.br>
2023-09-04 11:32:12 -0300

    feat: add folders structure and some configs

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,16 @@
-//  CÓDIGO BASE DO EXPRESS
+// CÓDIGO BASE DO EXPRESS
+// API REST CONSEGUE DISPONIBILIZAR CHAMADAS PARA ELA (ENDPOINTS) ATRAVÉS DO GET, POST, PUT E DELETE
 import express from "express";
+import "dotenv/config";
+// CRIANDO INSTANCIA
+import { router } from "./routes";
 
 const server = express();
-//
-server.get("/", (_, res) => {
-  return res.send("Hello World.");
-});
 
-//
+// IRÁ TRAZER OS DADOS EM JSON
+server.use(express.json());
+
+// O SERVIDOR IRÁ UTILIZAR AS ROTAS QUE ESTÃO NO ROUTER
+server.use(router);
+
 export { server };
